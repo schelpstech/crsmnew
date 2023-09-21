3d11ae7b1ce886e285a8a24eabc8dd1b8d53e8f1
schelpstech <sunkanmi@schelps.com.ng>
2023-09-21 17:22:02 +0100

    head tag update

--- include/header.php
+++ include/header.php
@@ -13,9 +13,12 @@ if (file_exists('../../model/query.php')) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
+<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>CRSM || Christ Redeemer's School Management</title>
+  <meta name="description" content="CRSM establishes, manages, supports and controls all RCCG Nursery, Primary and Secondary schools.">
+  <meta name="keywords" content="CRSM, CRSM Schools, RCCG Schools, RCCG, Schools in Nigeria, Schools in Lagos">
+  <meta name="author" content="CRSM || Christ Redeemer's School Management Board">
+	<title>CRSM || Christ Redeemer's School Management Board</title>
 <!-- google font -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
